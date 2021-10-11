5b8a5ed329528da6513d3228168486deb5675892
Sebastian Gubiec <seb.gub@wp.pl>
2021-10-12 00:00:04 +0200

    fix List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,7 +3,7 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
-import {settings} from '../../data/dataStore';
+import {settings, listData} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 import Creator from '../Creator/Creator.js';
 
@@ -20,6 +20,7 @@ class List extends React.Component {
 
   static defaultProps = {
     description: settings.defaultListDescription,
+    columns: listData.columns,
 }
 
   addColumn(title){

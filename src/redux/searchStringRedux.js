9fc6f44a13912d80a60dcc8a267e1f7ae08e703a
Sebastian Gubiec <seb.gub@wp.pl>
2021-11-12 13:55:03 +0100

    search component

---
+++ src/redux/searchStringRedux.js
@@ -0,0 +1,25 @@
+// selectors
+export const getSearchString = ({searchString}) => searchString;
+
+export const countVisibleCards= ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title));
+
+export const countAllCards = ({cards}) => cards.length;
+
+// action name creator
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// actions types
+export const CHANGE_SEARCH = createActionName('CHANGE_SEARCH');
+
+// action creators
+export const createAction_changeSearchString = searchString => ({ payload: searchString, type: CHANGE_SEARCH });
+// reducer
+export default function reducer(statePart = '', action = {}) {
+  switch (action.type) {
+    case CHANGE_SEARCH:
+      return [...statePart, action.payload];
+    default:
+      return statePart;
+  }
+}
